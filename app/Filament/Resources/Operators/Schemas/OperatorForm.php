4838854808f6248dac4f19ed41395a85f6410479
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-01-29 12:56:47 +0700

    fix: Remove unique constraint from user_id select field in OperatorForm

--- app/Filament/Resources/Operators/Schemas/OperatorForm.php
+++ app/Filament/Resources/Operators/Schemas/OperatorForm.php
@@ -32,7 +32,6 @@ class OperatorForm
                             ->preload()
                             ->searchable()
                             ->required()
-                            ->unique(),
                     ])->columnSpanFull()
             ]);
     }
